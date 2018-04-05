4225ba6a647621be42f76c0d13cd3f08999464b5
sonnyhcl <sonnyhcl@163.com>
2018-04-05 21:07:42 +0800

    add partial-build tcl

---
+++ build_mult_constant.tcl
@@ -0,0 +1,8 @@
+cd overlays
+source tutorial_2.tcl
+add_files -norecurse [make_wrapper -files [get_files "[current_bd_design].bd"] -top]
+update_compile_order -fileset sources_1
+set_property top tutorial_2_wrapper [current_fileset]
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+file copy -force tutorial_2/tutorial_2.runs/impl_1/tutorial_2_wrapper.bit tutorial_2.bit
---
+++ build_scalar_add.tcl
@@ -0,0 +1,8 @@
+cd overlays
+source tutorial_1.tcl
+add_files -norecurse [make_wrapper -files [get_files "[current_bd_design].bd"] -top]
+update_compile_order -fileset sources_1
+set_property top tutorial_1_wrapper [current_fileset]
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+file copy -force tutorial_1/tutorial_1.runs/impl_1/tutorial_1_wrapper.bit tutorial_1.bit
