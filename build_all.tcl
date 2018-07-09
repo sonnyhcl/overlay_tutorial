08333d26aa884b0ad4083449b1b9f5ec9c33f482
sonnyhcl <sonnyhcl@163.com>
2018-07-10 00:22:03 +0800

    fix build_all

--- build_all.tcl
+++ build_all.tcl
@@ -1,21 +1,11 @@
 cd ip
 foreach script [glob */script.tcl] { exec vivado_hls -f $script }
 
-cd ../overlays
-source tutorial_1.tcl
-add_files -norecurse [make_wrapper -files [get_files "[current_bd_design].bd"] -top]
-update_compile_order -fileset sources_1
-set_property top tutorial_1_wrapper [current_fileset]
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1
-file copy -force tutorial_1/tutorial_1.runs/impl_1/tutorial_1_wrapper.bit tutorial_1.bit
+cd ..
+cd overlays
+
+source build_scalar_add.tcl
 close_project
 
-source tutorial_2.tcl
-add_files -norecurse [make_wrapper -files [get_files "[current_bd_design].bd"] -top]
-update_compile_order -fileset sources_1
-set_property top tutorial_2_wrapper [current_fileset]
-launch_runs impl_1 -to_step write_bitstream
-wait_on_run impl_1
-file copy -force tutorial_2/tutorial_2.runs/impl_1/tutorial_2_wrapper.bit tutorial_2.bit
+source build_mult_constant.tcl
 close_project
